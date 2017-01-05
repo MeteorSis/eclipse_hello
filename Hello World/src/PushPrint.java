c1ea4415bb0d02910e1c0ff9043ea450d22bcf5e
Kijae Choi <uncleone@naver.com>
2017-01-05 20:10:16 +0900

    PushPrint.java modified on GitHub

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -2,5 +2,6 @@ class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Add Java Class File Push");
+		System.out.println("World with 42");
 	}
 }
