8fb939195e2384a09b93ff6ee52612e8791941b1
MeteorSis <uncleone@naver.com>
2017-01-05 18:56:28 +0900

    added print line

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -2,5 +2,6 @@ class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("World's End Dancehall");
 	}
 }
